77b5eb831bc8b84823559c2c0c6997ad3a696291
Kalyani Enupala <Kalyani.Enupala@apexon.com>
2025-01-30 21:43:02 +0530

    updated

--- src/main/java/com/ecommerce/OrderService/controller/OrderController.java
+++ src/main/java/com/ecommerce/OrderService/controller/OrderController.java
@@ -2,10 +2,12 @@ package com.ecommerce.OrderService.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("orders")
 public class OrderController {
 
     @Autowired
@@ -13,7 +15,7 @@ public class OrderController {
 
     @GetMapping("/orderStatus")
     public String getOrderStatus(){
-        restTemplate.getForObject("http://product-service/products",String.class);
-        return "Order Placed";
+        String response=restTemplate.getForObject("http://ProductService/products",String.class);
+        return "Order Placed"+response;
             }
 }
