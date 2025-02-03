c49cfc917f25c01d12f3b7094fa3d3552e55f4b3
Kalyani Enupala <Kalyani.Enupala@apexon.com>
2025-02-03 16:19:09 +0530

    configured Circuit Breaker

--- src/main/java/com/ecommerce/OrderService/controller/OrderController.java
+++ src/main/java/com/ecommerce/OrderService/controller/OrderController.java
@@ -1,6 +1,10 @@
 package com.ecommerce.OrderService.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +18,17 @@ public class OrderController {
     RestTemplate restTemplate;
 
     @GetMapping("/orderStatus")
-    public String getOrderStatus(){
-        System.out.println("gng inside order service");
-      String response=restTemplate.getForObject("http://ProductService/products",String.class);
-        return "Order Placed "+response;
-            }
+    @CircuitBreaker(name="product service",fallbackMethod = "fallbackProductService")
+    public ResponseEntity<String> getOrderStatus() {
+
+                    String response = restTemplate.getForObject("http://ProductService/products", String.class);
+                    return ResponseEntity.ok(response);
+
+    }
+
+    // Fallback method if ProductService is down
+    public ResponseEntity<String> fallbackProductService(Throwable ex) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("Product Service is temporarily down. Please try again later.");
+    }
 }
